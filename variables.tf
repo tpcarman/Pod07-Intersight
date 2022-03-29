80e192d5c411197bf94c485451cc3fc486e0c6f3
ist-training <85614799+ist-training@users.noreply.github.com>
2022-03-29 10:18:52 +0200

    Added variables.tf to lab 3

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "api_private_key" {
+  type = string
+}
+
+variable "api_key_id" {
+  type = string
+}
+
+variable "api_endpoint" {
+  type = string
+  default = "https://www.intersight.com"
+}
+
+variable "org_name" {
+  type = string
+  default = "default"
+}
+
