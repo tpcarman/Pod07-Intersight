a808856bfed050c9ac5dd551e07e17e5a7464d1e
ist-training <85614799+ist-training@users.noreply.github.com>
2022-03-31 05:40:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,22 @@ output "Pod07_organization_moid" {
   description = "organization id"
   value       = data.intersight_organization_organization.organization_moid.id
 }
+
+resource "Pod07_ntp "ntp1" {
+  name        = "ntp1"
+  description = "test policy"
+  enabled     = true
+  ntp_servers = [
+    "ntp.esl.cisco.com",
+    "time-a-g.nist.gov",
+    "time-b-g.nist.gov"
+  ]
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.organization_moid.id
+  }
+}
+variable "organization" {
+   type = string
+   description = "value for organization"
+ }
